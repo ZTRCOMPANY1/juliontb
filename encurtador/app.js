59b9df2f017174111e3a5d4c2f1a5d4cfee25577
ZTRCOMPANY1 <ztrcompany1@gmail.com>
2026-03-19 00:04:44 -0300

    asassaasa

--- encurtador/app.js
+++ encurtador/app.js
@@ -338,12 +338,12 @@ linksTableBody.addEventListener("click", async (event) => {
 
 logoutBtn.addEventListener("click", async () => {
   await signOut(auth);
-  window.location.href = "/login/";
+  window.location.href = "/login/index.html";
 });
 
 onAuthStateChanged(auth, async (user) => {
   if (!user) {
-    window.location.href = "/login/";
+    window.location.href = "/login/index.html";
     return;
   }
 
